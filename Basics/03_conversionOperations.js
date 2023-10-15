c1b80f79e071f86a03f12bb24ca0f3492f9bea40
amitbhadoria2001 <amitbhadoria001@gmail.com>
2023-10-15 14:43:16 +0000

    conversion of Datatypes

---
+++ Basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+//let score ="33abc"
+// let score = null
+// let score = undefined
+let score = true
+
+
+// const {score} = req.body //when we are taking the value of score from the frontend
+
+// console.log(typeof(score))
+// console.log(typeof(score01))
+// console.log(typeof(score02))
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+let isLoggedIn =1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn)
+
+// 1 = true 
+// 0 = false 
+// "" = false
+// "Amit" = true
+
+let num = 0
+
+let stringNumber = String(num)
+console.log(typeof stringNumber)
