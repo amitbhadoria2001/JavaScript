1f8b7c64066ed09c10acc792a94b4eee7c8d6210
amitbhadoria2001 <amitbhadoria001@gmail.com>
2023-10-15 14:06:25 +0000

    new commit till-02

--- Basics/01_variables.js
+++ Basics/01_variables.js
@@ -1,7 +1,14 @@
 const accountNo = 12345
 var name = "Amit"
 let emailId = "amitsingh@gmail.com"
+accountCity = "Betul"
 
-console.table([accountNo,name,emailId])
+let accountstate;
+
+
+// accountNo = 2
+
+console.log(accountstate)
+// console.table([accountNo,name,emailId])
 
 
---
+++ Basics/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; //treat all Js code as newer version
+
+// alert(3+2) //we are using node js not browser
+
+console.log(3+3); console.log("Amit");
+
+let name = "Amit"
+let age = 21
+let isLoggedIn = false 
+
+// primitive data type
+//number
+//bigInt
+//string = ""
+//boolean
+//null = standalone value
+// undefined = let state;
+// symbol = unique 
+
+//obejcts
+
+console.log(typeof "Amit")
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
+
+
+
