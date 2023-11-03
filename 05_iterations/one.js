257aeca45140adcc4ddf1d3513b4e58fba021951
amitbhadoria2001 <amitbhadoria001@gmail.com>
2023-11-03 06:54:23 +0000

    loops

---
+++ 05_iterations/one.js
@@ -0,0 +1,57 @@
+//for 
+
+for (let i = 1; i <= 10; i++) {
+    const element = i
+    //console.log(element)
+    
+}
+for(let i= 1; i <= 5; i++){
+    let pattern = ''
+    for(let j = i; j <= 5; j++){
+        pattern = pattern + '* '
+    }
+    // console.log(pattern)
+}
+// console.log("Amit")
+
+const myArr = ["Batman","GoldenBoy","Superman","Billy-Butcher"]
+for (let index = 0; index < myArr.length; index++) {
+    const element = myArr[index];
+    // console.log(element)
+}
+// console.log(`Best of all the four is ${myArr[3]}`)
+
+//break and Continue
+
+for (let i = 0; i < 10; i++) {
+   if (i == 5) {
+     console.log("5 is the best number")
+    break;
+   }    
+    // console.log(i)
+}
+
+
+//for continue 
+for (let i = 0; i < 10; i++) {
+    //console.log(i);
+   if (i == 5) {
+    //console.log("5 is the best number")
+    continue;
+   }    
+    //console.log(i)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 05_iterations/two.js
@@ -0,0 +1,25 @@
+//while and Do-while loop
+
+let i = 2
+
+while(i<10){
+    //console.log(i)
+    i+=2
+}
+
+let myArr = ["Batman","Superman","Spiderman"]
+let arr = 0
+
+while(arr < myArr.length){
+    //console.log(myArr[arr]);
+    arr++
+}
+
+
+//do while loop
+let score = 11;
+
+do{
+    console.log(`score is ${score}`)
+    score++
+}while(score<=10)
